6223b35074d18b142d26b9e10bd1c6f1ee1fdb96
Swap Portal <swapportalsry@gmail.com>
2019-07-13 16:10:18 +0530

    Commited project first time

---
+++ swapportal/src/com/Base.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Base {
+
+	public static void main(String[] args) {
+//first project
+		// TODO Auto-generated method stub
+System.out.println("8 sem project");
+
+	}
+
+}
